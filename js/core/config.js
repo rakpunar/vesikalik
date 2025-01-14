f43918de8b993302550be2c660e080ea2c121499
rakpunar <r.akpunar@gmail.com>
2025-01-14 16:38:03 +0300

    Create config.js

---
+++ js/core/config.js
@@ -0,0 +1,24 @@
+export const Config = {
+    APP: {
+        NAME: 'Vesikalık Fotoğraf',
+        VERSION: '1.0.0'
+    },
+    STORAGE: {
+        DB_NAME: 'VesikalikDB',
+        STORE_NAME: 'photos',
+        VERSION: 1,
+        MAX_PHOTOS: 25
+    },
+    PHOTO: {
+        WIDTH: 350,
+        HEIGHT: 450,
+        QUALITY: 0.9,
+        FORMAT: 'image/jpeg'
+    },
+    CAMERA: {
+        ASPECT_RATIO: {
+            WIDTH: 35,
+            HEIGHT: 45
+        }
+    }
+}; 
